0ad6f4431a69f9b44f2695100a42a26703a2036d
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-03-07 15:07:51 +1100

    Songs -> DishSongs

--- aus-ddr-api.Api/Models/Responses/Summer2021Event/DishResponse.cs
+++ aus-ddr-api.Api/Models/Responses/Summer2021Event/DishResponse.cs
@@ -9,7 +9,7 @@ namespace AusDdrApi.Models.Responses
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public IEnumerable<DishSongResponse> Songs { get; set; } = new HashSet<DishSongResponse>();
+        public IEnumerable<DishSongResponse> DishSongs { get; set; } = new HashSet<DishSongResponse>();
         public IEnumerable<IngredientResponse> Ingredients { get; set; } = new HashSet<IngredientResponse>();
         public string Image32 { get; set; } = string.Empty;
         public string Image64 { get; set; } = string.Empty;
@@ -20,7 +20,7 @@ namespace AusDdrApi.Models.Responses
         {
             Id = dish.Id,
             Name = dish.Name,
-            Songs = dish.DishSongs.Select(DishSongResponse.FromEntity),
+            DishSongs = dish.DishSongs.Select(DishSongResponse.FromEntity),
             Ingredients = dish.Ingredients.Select(IngredientResponse.FromEntity),
             Image32 = $"/summer2021/dishes/{dish.Id}.32.png",
             Image64 = $"/summer2021/dishes/{dish.Id}.64.png",

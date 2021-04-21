67a647ffd140983fd635b8b9bee1d07ac9418534
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-04-21 14:45:45 +1000

    Expose image for graded ingredients

--- Api/Entities/Summer2021Event/GradedIngredient.cs
+++ Api/Entities/Summer2021Event/GradedIngredient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AusDdrApi.Entities
 {
@@ -11,5 +12,14 @@ namespace AusDdrApi.Entities
         
         public Guid IngredientId { get; set; }
         public Ingredient? Ingredient { get; set; }
+
+        [NotMapped]
+        public string Image32 => $"/summer2021/gradedingredients/{Id}.32.png";
+        [NotMapped]
+        public string Image64 => $"/summer2021/gradedingredients/{Id}.64.png";
+        [NotMapped]
+        public string Image128 => $"/summer2021/gradedingredients/{Id}.128.png";
+        [NotMapped]
+        public string Image256 => $"/summer2021/gradedingredients/{Id}.256.png";
     }
 }

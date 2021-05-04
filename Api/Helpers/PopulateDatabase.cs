e782dab895aa940e7f9bc1aec6ee0f907fc112bf
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-05-04 17:03:47 +1000

    Add song difficulties to dummy data

--- Api/Helpers/PopulateDatabase.cs
+++ Api/Helpers/PopulateDatabase.cs
@@ -18,6 +18,7 @@ namespace AusDdrApi.Helpers
 
             await DropTables(context);
             await AddSongs(context);
+            await AddSongDifficulties(context);
         }
 
         private static async Task DropTables(DatabaseContext context)
@@ -74,6 +75,65 @@ SELECT truncate_schema('public');";
                 },
             });
 
+            await context.SaveChangesAsync();
+        }
+
+        private static async Task AddSongDifficulties(DatabaseContext context)
+        {
+            await context.AddRangeAsync(new List<SongDifficulty>
+            {
+                new()
+                {
+                    Id = Guid.Parse("22210e1e-f8fc-484d-a5e4-5680ff84d6b5"),
+                    SongId = Guid.Parse("f47d376b-67d7-4789-b880-371ca9ea5da8")
+                },
+                new()
+                {
+                    Id = Guid.Parse("e715e642-dc8d-4b3e-bde5-aeda22f4cc18"),
+                    SongId = Guid.Parse("1fe2344a-dde0-466f-aa76-2c9784783670")
+                },
+                new()
+                {
+                    Id = Guid.Parse("89321afb-cf74-4907-871b-0228487b3889"),
+                    SongId = Guid.Parse("1fe2344a-dde0-466f-aa76-2c9784783670")
+                },
+                new()
+                {
+                    Id = Guid.Parse("5d5186ce-661e-4f55-bd10-84eb614cf2ce"),
+                    SongId = Guid.Parse("b005f39f-de77-4939-965f-9852a1ed9efc")
+                },
+                new()
+                {
+                    Id = Guid.Parse("0565d83b-44fd-4780-b4e9-47fd765e837b"),
+                    SongId = Guid.Parse("b005f39f-de77-4939-965f-9852a1ed9efc")
+                },
+                new()
+                {
+                    Id = Guid.Parse("7506966e-9307-435a-8671-d655487a1221"),
+                    SongId = Guid.Parse("b005f39f-de77-4939-965f-9852a1ed9efc")
+                },
+                new()
+                {
+                    Id = Guid.Parse("b5fda847-c8b7-47a8-bef6-3e7a069e342f"),
+                    SongId = Guid.Parse("0665bd6e-a39c-417e-9be1-4e34c6eca781")
+                },
+                new()
+                {
+                    Id = Guid.Parse("239ec546-7c0e-40ec-8fd9-391fd9a27d60"),
+                    SongId = Guid.Parse("0665bd6e-a39c-417e-9be1-4e34c6eca781")
+                },
+                new()
+                {
+                    Id = Guid.Parse("0a62587e-0c30-4d8a-a6c6-e103a21d8db1"),
+                    SongId = Guid.Parse("0665bd6e-a39c-417e-9be1-4e34c6eca781")
+                },
+                new()
+                {
+                    Id = Guid.Parse("c124199e-2645-4467-b413-97ad1b9c4997"),
+                    SongId = Guid.Parse("0665bd6e-a39c-417e-9be1-4e34c6eca781")
+                }
+            });
+            
             await context.SaveChangesAsync();
         }
     }

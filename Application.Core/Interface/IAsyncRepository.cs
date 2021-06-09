fef1d80059896edb1bec266ed4addfb4fbda21a4
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-06-09 18:48:42 +1000

    Add initial migration

--- Application.Core/Interface/IAsyncRepository.cs
+++ Application.Core/Interface/IAsyncRepository.cs
@@ -1,20 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading;
-using System.Threading.Tasks;
 using Application.Core.Entities;
+using Ardalis.Specification;
 
 namespace Application.Core.Interface
 {
-    public interface IAsyncRepository<T> where T : BaseEntity
+    public interface IAsyncRepository<T> : IRepositoryBase<T> where T : BaseEntity
     {
-        Task<T?> GetById(Guid id, CancellationToken cancellationToken = default);
-        Task<IReadOnlyList<T>> List(CancellationToken cancellationToken = default);
-        Task<IReadOnlyList<T>> List(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
-        Task Add(T entity, CancellationToken cancellationToken = default);
-        Task Delete(T entity, CancellationToken cancellationToken = default);
-        Task Edit(T entity, CancellationToken cancellationToken = default);
     }
 }
--- Infrastructure/Data/GenericEfRepository.cs
+++ Infrastructure/Data/GenericEfRepository.cs
@@ -1,51 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading;
-using System.Threading.Tasks;
 using Application.Core.Entities;
 using Application.Core.Interface;
+using Ardalis.Specification.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class GenericEfRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    public class GenericEfRepository<T> : RepositoryBase<T>, IAsyncRepository<T> where T : BaseEntity
     {
-        protected readonly EFDatabaseContext _dbContext;
-
-        public GenericEfRepository(EFDatabaseContext dbContext)
+        public GenericEfRepository(EFDatabaseContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
-        }
-
-        public async Task<T?> GetById(Guid id, CancellationToken cancellationToken = default)
-        {
-            var keyValues = new object[] {id};
-            return await _dbContext.Set<T>().FindAsync(keyValues, cancellationToken);
-        }
-
-        public Task<IReadOnlyList<T>> List(CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IReadOnlyList<T>> List(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Add(T entity, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Delete(T entity, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Edit(T entity, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
         }
     }
 }

6e8b87472b68ff5f351d264853f86730ba515366
Christopher Shand-Gost <christopher.shandgost@myob.com>
2021-06-17 16:31:50 +1000

    Use a generic spec for get by id / list

--- Application.Core/Services/DancerService.cs
+++ Application.Core/Services/DancerService.cs
@@ -4,7 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Core.Entities;
 using Application.Core.Interfaces;
-using Application.Core.Specifications.DancerSpecs;
+using Application.Core.Specifications;
 using Ardalis.Result;
 
 namespace Application.Core.Services
@@ -20,7 +20,7 @@ namespace Application.Core.Services
         
         public async Task<Result<Dancer>> GetDancerByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            var dancerSpec = new DancerByIdSpec(id);
+            var dancerSpec = new ByIdSpec<Dancer>(id);
             var dancer = await _repository.GetBySpecAsync(dancerSpec, cancellationToken);
             return dancer == null ? Result<Dancer>.NotFound() : Result<Dancer>.Success(dancer);
         }
@@ -28,7 +28,7 @@ namespace Application.Core.Services
         public async Task<Result<IList<Dancer>>> GetDancersAsync(int page, int limit, CancellationToken cancellationToken)
         {
             var skip = page * limit;
-            var dancersSpec = new DancersSpec(skip, limit);
+            var dancersSpec = new PageableSpec<Dancer>(skip, limit);
             var dancers = await _repository.ListAsync(dancersSpec, cancellationToken);
             return Result<IList<Dancer>>.Success(dancers);
         }
--- Application.Core/Specifications/PageableSpec.cs
+++ Application.Core/Specifications/PageableSpec.cs
@@ -4,6 +4,13 @@ using Ardalis.Specification;
 
 namespace Application.Core.Specifications
 {
+    /// <summary>
+    /// PageableSpec<T> intends to exist as a basic `List` spec implementation
+    /// with simple paging capabilities. If you require further entity context
+    /// such as children or filtering, or more complex paging capabilities, you
+    /// can inherit from this spec.
+    /// </summary>
+    /// <typeparam name="T">A root entity with an ID</typeparam>
     public class PageableSpec<T> : Specification<T> where T : BaseEntity, IAggregateRoot
     {
         public PageableSpec(int skip, int limit)

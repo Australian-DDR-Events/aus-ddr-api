f0a47e43265985899ed5ef2738e8d3704762e692
Chris-SG <chris.shandgost@gmail.com>
2022-07-09 16:29:15 +1000

    Remove unused file

--- Api/Endpoints/DancerEndpoints/DancerRecord.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace AusDdrApi.Endpoints.DancerEndpoints
-{
-    public record DancerRecord(Guid id, string name, string code, string primaryMachine, string state, string profilePictureUrl);
-}
